c9d8b6e0f13634be2b1aac60db5ec91758a763fa
sokhiennn <22013179@myrp.edu.sg>
2023-06-01 15:25:36 +0800

    Enhancement 1 half done

--- app/src/main/java/sg/edu/rp/c346/id22013179/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22013179/simpletodo/MainActivity.java
@@ -3,6 +3,8 @@ package sg.edu.rp.c346.id22013179.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -31,6 +33,26 @@ public class MainActivity extends AppCompatActivity {
         buttonClear = findViewById(R.id.buttonClear);
         listViewTasks = findViewById(R.id.ListViewTasks);
 
-        
+        alTask.add("Watch movie with Jenny");
+
+        ArrayAdapter aaTask = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alTask);
+        listViewTasks.setAdapter(aaTask);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = editTextTask.getText().toString();
+                alTask.add(task);
+                aaTask.notifyDataSetChanged();
+            }
+        });
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = editTextTask.getText().toString();
+                alTask.remove(task);
+                aaTask.notifyDataSetChanged();
+            }
+        });
     }
 }
